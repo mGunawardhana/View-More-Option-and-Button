1ea100d557116db077d68cccd7053380b7a8223d
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-07-14 12:16:47 +0530

    developed theme.kt file and config project

--- app/src/main/java/com/example/jetpackcomposeviewmoreoptionsample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeviewmoreoptionsample/MainActivity.kt
@@ -1,28 +1,23 @@
+package com.example.jetpackcomposeviewmoreoptionsample
+
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.codelab.basics.ui.BasicsCodelabTheme
+import com.example.jetpackcomposeviewmoreoptionsample.ui.theme.JetPackComposeViewMoreOptionSampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicsCodelabTheme {
+            JetPackComposeViewMoreOptionSampleTheme {
                 MyApp()
             }
         }
@@ -41,45 +36,21 @@ fun MyApp() {
     }
 }
 
-@Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
 
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onContinueClicked
-            ) {
-                Text("Continue")
+@Composable
+fun Greetings(names: List<String> = listOf("Android", "Compose")) {
+    Surface(color = MaterialTheme.colors.background) {
+        Column(modifier = Modifier.padding(vertical = 4.dp)) {
+            for (name in names) {
+                Greeting(name)
             }
         }
     }
+
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
-        }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    BasicsCodelabTheme {
-        OnboardingScreen(onContinueClicked = {})
-    }
-}
-
-@Composable
-private fun Greeting(name: String) {
+fun Greeting(name: String) {
 
     val expanded = remember { mutableStateOf(false) }
 
@@ -87,12 +58,13 @@ private fun Greeting(name: String) {
 
     Surface(
         color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
@@ -105,16 +77,41 @@ private fun Greeting(name: String) {
         }
     }
 }
-@Preview(
-    showBackground = true,
-    widthDp = 320,
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
-)
+
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the mGunawardhana's 1 st App!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    JetPackComposeViewMoreOptionSampleTheme{
+        OnboardingPreview()
+    }
+}
+
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
-    BasicsCodelabTheme {
-        Greetings()
+    JetPackComposeViewMoreOptionSampleTheme {
+        MyApp()
+
     }
 }
